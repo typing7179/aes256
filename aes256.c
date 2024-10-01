db954cdc93ccb8f3331c995a01d1ee404f76a496
typing7179 <156127806+typing7179@users.noreply.github.com>
2024-09-30 23:20:34 -0500

    AES-256 type c

--- aes256.c
+++ aes256.c
@@ -1,23 +1,6 @@
-//
-// A compact byte-oriented AES-256 implementation.
-// All lookup tables replaced with 'on the fly' calculations.
-//
-// Copyright (c) 2007-2011 Literatecode, http://www.literatecode.com
-// Copyright (c) 2022 Ilia Levin (ilia@levin.sg)
-//
-// Other contributors: Hal Finney.
-//
-// Permission to use, copy, modify, and distribute this software for any
-// purpose with or without fee is hereby granted, provided that the above
-// copyright notice and this permission notice appear in all copies.
-//
-// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// this is typing account 
+// images and more 
+
 
 #include "aes256.h"
 #ifndef NULL
